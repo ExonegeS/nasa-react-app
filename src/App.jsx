a178a766ec2ae40e7b091b69c80707abe3a81a77
ExonegeS <2004gusak@gmail.com>
2024-11-18 05:54:43 +0500

    "Added caching to API data using localStorage"

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,9 @@ import SideBar from "./components/SideBar"
 import { useEffect, useState } from "react"
 
 function App() {
-    const [data, setData] = useState({ date: "2024-11-17", explanation: "What is the cause of this unusual parabolic structure?  This illuminated cavity, known as LDN 1471, was created by a newly forming star, seen as the bright source at the peak of the parabola.  This protostar is experiencing a stellar outflow which is then interacting with the surrounding material in the Perseus Molecular Cloud, causing it to brighten.  We see only one side of the cavity -- the other side is hidden by dark dust.  The parabolic shape is caused by the widening of the stellar-wind blown cavity over time. Two additional structures can also be seen either side of the protostar; these are known as Herbig-Haro objects, again caused by the interaction of the outflow with the surrounding material.  What causes the striations on the cavity walls, though, remains unknown.  The featured image was taken by NASA and ESA’s Hubble Space Telescope after an original detection by the Spitzer Space Telescope.   Explore Your Universe: Random APOD Generator", hdurl: "https://apod.nasa.gov/apod/image/2411/LDN1471_HubbleSchmidt_1024.jpg", media_type: "image", service_version: "v1", title: "LDN 1471: A Windblown Star Cavity", url: "https://apod.nasa.gov/apod/image/2411/LDN1471_HubbleSchmidt_960.jpg" })
+    const [data, setData] = useState(null)
     const [loading, setLoading] = useState(false)
     const [showModel, setShowModel] = useState(false)
-
-
 
     function handleToggleModel() {
         setShowModel(!showModel)
@@ -19,18 +17,28 @@ function App() {
             const url = 'https://api.nasa.gov/planetary/apod' + 
             `?api_key=${NASA_API}`
 
+            const today = (new Date).toDateString()
+            const localKey = `NASA-${today}`
+            if (localStorage.getItem(localKey)) {
+                const apiData = JSON.parse(localStorage.getItem(localKey))
+                setData(apiData)
+                console.log("Fetched from cache today")
+                return
+            }
+            localStorage.clear()
             try {
                 const res = await fetch(url)
                 const apiData = await res.json()
+                localStorage.setItem(localKey, JSON.stringify(apiData))
                 setData(apiData)
-                console.log("DATA\n",apiData)
+                console.log("Fetched from API today")
             }
             catch (err) {
                 console.error(err)
             }
         }
 
-        // fetchAPIData()
+        fetchAPIData()
 
     }, [])
 
